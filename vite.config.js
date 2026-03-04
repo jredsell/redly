ad43dd377f3d758ecffd8bc2278e669e5baa16a0
jredsell <jonathan.redsell@gmail.com>
2026-03-04 13:59:02 +0000

    config

--- vite.config.js
+++ vite.config.js
@@ -41,6 +41,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        navigateFallbackDenylist: [/^\/redly\/docs/],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
